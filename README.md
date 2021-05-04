ac57384d82e665fae983e606568d19c181021c10
camh <cam.henlin@gmail.com>
2021-05-03 23:00:26 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# TESample
+
+Macintosh Toolbox TESample, working with Retro68. Displays a window with text editing capabilities. Not trivial with the toolbox!
+
+Updated from source code at https://github.com/fruitsamples/TESample
+
---
+++ build/CMakeFiles/TESample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TESample.dir/TESample.c.obj"
+  "TESample.code.bin"
+  "TESample.code.bin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TESample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
